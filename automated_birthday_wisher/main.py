893cba522ec346f750708f7c78440b4776d38f5e
Someet Singh <mrsingh.someet@gmail.com>
2022-08-05 16:26:20 +0530

    second update

--- automated_birthday_wisher/main.py
+++ automated_birthday_wisher/main.py
@@ -1,51 +1,45 @@
-
-# import smtplib
-
-# import os
-
-# from dotenv import load_dotenv
-# load_dotenv()
-
-# my_email = os.environ['my_email']
-# password = os.environ['password']
-
-# with smtplib.SMTP("smtp.gmail.com") as cn:
-#     cn.starttls()
-#     cn.login(user=my_email, password=password)
-#     cn.sendmail(from_addr=my_email
-#     , to_addrs=my_email,
-#      msg="Subject:hello \n\n this is the body of message---> happy birthday",)
-#     cn.close()
-
-
-
-import random
+from logging import exception
+from multiprocessing import connection
+import pandas as pd
 import datetime as dt
+import random
 import smtplib
-import os
-
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-now = dt.datetime.now()
-weekday = now.weekday()
-
-if weekday == 4:
-
-    with open("/home/someetsingh/python_projects/automated_birthday_wisher/quotes.txt") as quote_file:
-        all_quotes = quote_file.readlines()
-        quote = random.choice(all_quotes)
-    
-    
-
-    with smtplib.SMTP('smtp.gmail.com') as con:
-        con.starttls()
-        con.login(user=os.environ['my_email'], password=os.environ['password'])
-        con.sendmail(from_addr=os.environ['my_email'], to_addrs=os.environ['to_email'], msg=f"Subject:Friday Motivation\n\n{quote}")
-        print("message sent successfully")
+today = dt.datetime.now()
+today_tuple = (today.month, today.day)
+data = pd.read_csv("/home/someetsingh/python_projects/automated_birthday_wisher/birthdays.csv")
 
 
+birthday_dict = {(data_rows['month'], data_rows['day']): data_rows for (index,data_rows) in data.iterrows()}
+try:
+    if today_tuple in birthday_dict:
+        birthday_person = birthday_dict[today_tuple]
+        print(birthday_person)
+        file_path = f"/home/someetsingh/python_projects/automated_birthday_wisher/letter_templates/letter_" + str(random.randint(1,3)) + ".txt"
+        with open("/home/someetsingh/python_projects/automated_birthday_wisher/quotes.txt") as quote_file:
+            all_quotes = quote_file.readlines()
+            quote = random.choice(all_quotes)
+        with open(file_path) as file:
+            content = file.read()
+            content = content.replace("[NAME]", birthday_person['name']) 
+            content = content.replace("Angela", "Sunny") + "\n\n" + quote
+            print(content)
+        with smtplib.SMTP("smtp.gmail.com") as conn:
+            conn.starttls()
+            conn.login(user=os.environ['my_email'], password=os.environ['password'])
+            conn.sendmail(from_addr=os.environ['my_email'], to_addrs=os.environ['my_email'], msg=f"Subject:happy birthday\n\n{content}")
+            print(f"message successdully sent to {birthday_person['name']}")
+
+        
+except Exception as e:
+    print(f"the problem is with {Exception}")
+
+
+        
 
 
 
---
+++ automated_birthday_wisher/test.py
@@ -0,0 +1,41 @@
+
+# import smtplib
+
+# import os
+
+# from dotenv import load_dotenv
+# load_dotenv()
+
+# my_email = os.environ['my_email']
+# password = os.environ['password']
+
+# with smtplib.SMTP("smtp.gmail.com") as cn:
+#     cn.starttls()
+#     cn.login(user=my_email, password=password)
+#     cn.sendmail(from_addr=my_email
+#     , to_addrs=my_email,
+#      msg="Subject:hello \n\n this is the body of message---> happy birthday",)
+#     cn.close()
+
+# import random
+# import datetime as dt
+# import smtplib
+# import os
+# from dotenv import load_dotenv
+# load_dotenv()
+# now = dt.datetime.now()
+# weekday = now.weekday()
+# if weekday == 4:
+#     with open("/home/someetsingh/python_projects/automated_birthday_wisher/quotes.txt") as quote_file:
+#         all_quotes = quote_file.readlines()
+#         quote = random.choice(all_quotes)
+#     with smtplib.SMTP('smtp.gmail.com') as con:
+#         con.starttls()
+#         con.login(user=os.environ['my_email'], password=os.environ['password'])
+#         con.sendmail(from_addr=os.environ['my_email'], to_addrs=os.environ['to_email'], msg=f"Subject:Friday Motivation\n\n{quote}")
+#         print("message sent successfully")
+
+
+
+
+
